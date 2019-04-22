959fc79f9f0d5895e88b6d7a58ed6c9f15df6023
Sarahlee671 <sarah.lee1987@hotmail.com>
2019-04-22 13:00:03 -0700

    Created HeaderTitle component

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import './Header.css';
+import moment from 'moment';
+
+const HeaderTitle = () => {
+    return (
+        <div>
+            <h3>Lambda School</h3>
+            <p>@lambdaSchool</p>
+            <p>{moment().format('MMMM Do')}</p>
+        </div>
+    );
+};
+
+export default HeaderTitle;

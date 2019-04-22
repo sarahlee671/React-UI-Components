2fc8fa4a2272cec7e31466e1c45f522c3b7bbe41
Sarahlee671 <sarah.lee1987@hotmail.com>
2019-04-22 13:13:00 -0700

    Created CardContent component

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Card.css';
+
+const CardContent = () => {
+    return (
+        <div>
+            <p>Get started with React</p>
+            <p>React makes it painless to create interactive UIs. Design simple views for each state in your application.</p>
+        </div>
+    );
+};
+
+export default CardContent;
